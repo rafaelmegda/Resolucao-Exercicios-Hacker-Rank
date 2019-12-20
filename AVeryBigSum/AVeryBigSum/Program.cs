823602121de2047aaaf864aef45604095122f496
Rafael Megda <rafaelmisidoro@gmail.com>
2019-12-20 18:07:44 -0300

    Add A Very Big Sum

---
+++ AVeryBigSum/AVeryBigSum/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the aVeryBigSum function below.
+    static long aVeryBigSum(long[] ar, int arCount)
+    {
+        long soma = 0;
+        for (var x = 0; x <= arCount - 1; x++)
+        {
+            soma += ar[x];
+        }
+        return soma;
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int arCount = Convert.ToInt32(Console.ReadLine());
+
+        long[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt64(arTemp))
+        ;
+        long result = aVeryBigSum(ar, arCount);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
+
+
+
